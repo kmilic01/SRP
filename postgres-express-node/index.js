f91c8c0f0866e8a2e92a97b98ee50d5272566649
kmilic01 <kmilic01@fesb.hr>
2020-10-28 00:08:05 +0100

    Test file authentication

--- postgres-express-node/index.js
+++ postgres-express-node/index.js
@@ -103,18 +103,17 @@ async function init() {
    */
   try {
     const secret = "my supper secret";
-    let message = "Authenticate this message.";
+    /*let message = "Authenticate this message.";
 
     let hmac = crypto.createHmac("sha256", secret); // ! Message authentication code
     let authTag = hmac.update(message).digest(); // ! Message digest/authentication code
     console.table([
       { message, "message digest/authentication tag": authTag.toString("hex") },
-    ]);
+    ]);*/
 
     // * ===============================
     // * Example: Authenticating a file
     // *--------------------------------
-    /*
     const CREATE_TAG = false;
 
     if (CREATE_TAG) {
@@ -146,7 +145,6 @@ async function init() {
         );
       });
     }
-    */
   } catch (err) {
     console.log(err.message);
   }
